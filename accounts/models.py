017ab4be19688c699afd664d948d542689414b3e
su <mini72667@gmail.com>
2023-11-22 11:54:04 +0900

    프로젝트 세팅

---
+++ accounts/models.py
@@ -0,0 +1,21 @@
+# accounts/models.py
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
+from .managers import CustomUserManager
+
+
+class CustomUser(AbstractUser):
+    username = None
+    email = models.EmailField(_('email address'), unique=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    date_of_birth = models.DateField(blank=True, null=True)
+    
+
+    def __str__(self):
+        return self.email
